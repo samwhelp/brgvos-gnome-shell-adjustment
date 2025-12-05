cb70d19d4ef943db76a0395f9ffa3a6dd6ea1751
samwhelp <samwhelp@users.noreply.github.com>
2025-12-05 11:03:50 +0000

    fix

---
+++ demo/scripts/appearance/layout/Advance/WinCity/gnome-shell-layout-wincity.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+
+##
+## ## Gnome Shell / Config / Layout
+##
+
+mod_gnome_shell_config_for_layout () {
+
+
+	return 0
+}
+
+
+##
+## ## Gnome Shell / Config
+##
+
+mod_gnome_shell_config () {
+
+
+
+
+	mod_gnome_shell_config_for_layout
+
+
+
+
+	return 0
+}
+
+mod_config_portal () {
+
+
+	echo
+	echo "##"
+	echo "## ## Gnome Shell Config / Layout / Start"
+	echo "##"
+	echo
+
+
+
+
+	mod_gnome_shell_config
+
+
+
+
+	echo
+	echo "##"
+	echo "## ## Gnome Shell Config / Layout / Done"
+	echo "##"
+	echo
+
+
+	return 0
+}
+
+
+
+
+##
+## ## Main
+##
+
+__main__ () {
+
+	mod_config_portal
+
+	return 0
+}
+
+__main__ "${@}"
