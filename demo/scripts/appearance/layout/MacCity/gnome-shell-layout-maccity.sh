9e1c274bab3ca20efe5f10105eec62bde7bb236d
samwhelp <samwhelp@users.noreply.github.com>
2025-12-05 10:16:33 +0000

    fix

---
+++ demo/scripts/appearance/layout/MacCity/gnome-shell-layout-maccity.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+
+
+
+##
+## ## Disable Extensions
+##
+
+gnome-extensions disable 'mediacontrols@cliffniff.github.com'
+gnome-extensions disable 'arcmenu@arcmenu.com'
+gnome-extensions disable 'dash-to-panel@jderose9.github.com'
+gnome-extensions disable 'tilingshell@ferrarodomenico.com'
+
+
+
+
+##
+## ## Enable Extensions
+##
+
+gnome-extensions enable 'dash-to-dock@micxgx.gmail.com'
+gnome-extensions enable 'logomenu@aryan_k'
+gnome-extensions enable 'space-bar@luchrioh'
+gnome-extensions enable 'tiling-assistant@leleat-on-github'
+
+
+
+
+##
+## ## Window Botton Layout
+##
+
+#gsettings set org.gnome.desktop.wm.preferences button-layout "'appmenu:minimize,maximize,close'"
+
+gsettings set org.gnome.desktop.wm.preferences button-layout "'close,minimize,maximize:appmenu'"
+
+
+
+
+##
+## ## Extension / dash-to-panel
+##
+
+dconf write /org/gnome/shell/extensions/dash-to-panel/panel-lengths '{"AUO-0x00000000":100,"unknown-unknown":100}'
+dconf write /org/gnome/shell/extensions/dash-to-panel/panel-positions '{"AUO-0x00000000":"TOP","unknown-unknown":"TOP"}'
+dconf write /org/gnome/shell/extensions/dash-to-panel/panel-sizes '{"AUO-0x00000000":48}'
+
+
+
+
+##
+## ## Extension / dash-to-panel
+##
+
+dconf write /org/gnome/shell/extensions/dash-to-dock/dock-position "'BOTTOM'"
+
+
+
+
+##
+## ## Extension / simple-weather
+##
+
+dconf write /org/gnome/shell/extensions/simple-weather/panel-box "'center'"
+dconf write /org/gnome/shell/extensions/simple-weather/main-location-index "int64 0"
+dconf write /org/gnome/shell/extensions/simple-weather/panel-priority "int64 0"
+
+
+
+
+##
+## ## Extension / space-bar
+##
+
+dconf write /org/gnome/shell/extensions/space-bar/appearance/active-workspace-text-color "'rgb(255,255,255)'"
+dconf write /org/gnome/shell/extensions/space-bar/appearance/inactive-workspace-text-color "'rgb(222,221,218)'"
+
---
+++ demo/scripts/appearance/layout/WinCity/gnome-shell-layout-wincity.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+
+
+
+##
+## ## Disable Extensions
+##
+
+gnome-extensions disable 'dash-to-dock@micxgx.gmail.com'
+gnome-extensions disable 'logomenu@aryan_k'
+gnome-extensions disable 'space-bar@luchrioh'
+gnome-extensions disable 'tiling-assistant@leleat-on-github'
+
+
+
+
+##
+## ## Enable Extensions
+##
+
+gnome-extensions enable 'mediacontrols@cliffniff.github.com'
+gnome-extensions enable 'arcmenu@arcmenu.com'
+gnome-extensions enable 'dash-to-panel@jderose9.github.com'
+gnome-extensions enable 'tilingshell@ferrarodomenico.com'
+
+
+
+
+##
+## ## Window Botton Layout
+##
+
+gsettings set org.gnome.desktop.wm.preferences button-layout "'appmenu:minimize,maximize,close'"
+
+
+
+
+##
+## ## Extension / dash-to-panel
+##
+
+dconf write /org/gnome/shell/extensions/dash-to-panel/panel-lengths '{"AUO-0x00000000":70,"unknown-unknown":70}'
+dconf write /org/gnome/shell/extensions/dash-to-panel/panel-positions "'{}'"
+dconf write /org/gnome/shell/extensions/dash-to-panel/panel-sizes '{"AUO-0x00000000":48}'
+
+
+
+
+##
+## ## Extension / dash-to-dock
+##
+
+dconf write /org/gnome/shell/extensions/dash-to-dock/dock-position "'TOP'"
+
+
+
+
+##
+## ## Extension / simple-weather
+##
+
+dconf write /org/gnome/shell/extensions/simple-weather/panel-box "'left'"
+dconf write /org/gnome/shell/extensions/simple-weather/main-location-index "int64 0"
+
