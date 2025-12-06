1e348eaa0d6091ea9dc2490554bdd1c7620e568c
samwhelp <samwhelp@users.noreply.github.com>
2025-12-06 20:24:04 +0800

    fix

---
+++ demo/scripts/appearance/color-scheme/config-color-scheme-dark.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+
+
+
+##
+## ## config via gsettings
+##
+
+gsettings set org.gnome.desktop.interface color-scheme "'prefer-dark'"
+
+
+
+
+##
+## ## config via dconf
+##
+
+#dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
+
